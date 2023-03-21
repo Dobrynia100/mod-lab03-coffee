1ce18c96bc9b15fa8e6cf314145c5df77e7e0af2
Dobrynia100 <58748374+Dobrynia100@users.noreply.github.com>
2023-03-21 19:12:56 +0300

    Косметические test

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,38 +4,37 @@
 #include "Automata.h"
 
 TEST(task1, test1) {
-	Automata coffee = Automata();
-	coffee.on();	
-	enum STATES { WAIT };
-	EXPECT_EQ(WAIT, coffee.getState());
+ Automata coffee = Automata();
+ coffee.on();
+ enum STATES { WAIT };
+ EXPECT_EQ(WAIT, coffee.getState());
 }
 
 TEST(task1, test2) {
-	Automata coffee = Automata();
-	coffee.on();
-	coffee.getMenu();
-	enum STATES { ACCEPT };
-	EXPECT_EQ(ACCEPT, coffee.getState());
+ Automata coffee = Automata();
+ coffee.on();
+ coffee.getMenu();
+ enum STATES { ACCEPT };
+ EXPECT_EQ(ACCEPT, coffee.getState());
 }
 
 TEST(task1, test3) {
-	Automata coffee = Automata();
-	coffee.on();
-	coffee.getMenu();
-	coffee.choice(1);
-	enum STATES { CHECK };
-	EXPECT_EQ(CHECK, coffee.getState());
+ Automata coffee = Automata();
+ coffee.on();
+ coffee.getMenu();
+ coffee.choice(1);
+ enum STATES { CHECK };
+ EXPECT_EQ(CHECK, coffee.getState());
 }
 
 TEST(task1, test4) {
-	Automata coffee = Automata();
-	coffee.on();
-	coffee.getMenu();
-	coffee.choice(1);
-	coffee.coin(100);
-	
-	enum STATES { CHECK };
-	EXPECT_EQ(CHECK, coffee.getState());
+ Automata coffee = Automata();
+ coffee.on();
+ coffee.getMenu();
+ coffee.choice(1);
+ coffee.coin(100);
+ enum STATES { CHECK };
+ EXPECT_EQ(CHECK, coffee.getState());
 }
 
 TEST(task1, test5) {
@@ -51,59 +50,57 @@ TEST(task1, test5) {
 }
 
 TEST(task1, test6) {
-	Automata coffee = Automata();
-	coffee.on();
-	coffee.getMenu();
-	coffee.choice(1);
-	coffee.coin(100);
-	coffee.check();
-	coffee.cook();
-	
-	enum STATES { COOK };
-	EXPECT_EQ(COOK, coffee.getState());
+ Automata coffee = Automata();
+ coffee.on();
+ coffee.getMenu();
+ coffee.choice(1);
+ coffee.coin(100);
+ сoffee.check();
+ coffee.cook();
+ enum STATES { COOK };
+ EXPECT_EQ(COOK, coffee.getState());
 }
 TEST(task1, test7) {
-	Automata coffee = Automata();
-	coffee.on();
-	coffee.getMenu();
-	coffee.choice(1);
-	coffee.coin(10);
-
-	enum STATES { CHECK };
-	EXPECT_EQ(CHECK, coffee.getState());
+ Automata coffee = Automata();
+ coffee.on();
+ coffee.getMenu();
+ coffee.choice(1);
+ coffee.coin(10);
+ enum STATES { CHECK };
+ EXPECT_EQ(CHECK, coffee.getState());
 }
 TEST(task1, test8) {
-	Automata coffee = Automata();
-	coffee.on();
-	coffee.getMenu();
-	coffee.choice(1);
-	coffee.coin(100);
-	coffee.check();
-	coffee.cook();
-	coffee.finish();
-	enum STATES { WAIT };
-	EXPECT_EQ(WAIT, coffee.getState());
+ Automata coffee = Automata();
+ coffee.on();
+ coffee.getMenu();
+ coffee.choice(1);
+ coffee.coin(100);
+ coffee.check();
+ coffee.cook();
+ coffee.finish();
+ enum STATES { WAIT };
+ EXPECT_EQ(WAIT, coffee.getState());
 }
 TEST(task1, test9) {
-	Automata coffee = Automata();
-	coffee.on();
-	coffee.getMenu();
-	coffee.choice(1);
-	coffee.coin(100);
-	coffee.cancel();
-	enum STATES { WAIT };
-	EXPECT_EQ(WAIT, coffee.getState());
+ Automata coffee = Automata();
+ coffee.on();
+ coffee.getMenu();
+ coffee.choice(1);
+ coffee.coin(100);
+ coffee.cancel();
+ enum STATES { WAIT };
+ EXPECT_EQ(WAIT, coffee.getState());
 }
 TEST(task1, test10) {
-	Automata coffee = Automata();
-	coffee.on();
-	coffee.getMenu();
-	coffee.choice(1);
-	coffee.coin(100);
-	coffee.cancel();
-	coffee.off();
-	enum STATES { OFF };
-	EXPECT_EQ(OFF, coffee.getState());
+ Automata coffee = Automata();
+ coffee.on();
+ coffee.getMenu();
+ coffee.choice(1);
+ сoffee.coin(100);
+ coffee.cancel();
+ coffee.off();
+ enum STATES { OFF };
+ EXPECT_EQ(OFF, coffee.getState());
 }
 
 
